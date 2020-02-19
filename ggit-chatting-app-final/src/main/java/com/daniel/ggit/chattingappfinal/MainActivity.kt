dab475299a370d388634f205da9ccfe627eec855
daniel <danielawde9@gmail.com>
2020-02-19 14:04:54 +0200

    Initial commit

---
+++ ggit-chatting-app-final/src/main/java/com/daniel/ggit/chattingappfinal/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.daniel.ggit.chattingappfinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
